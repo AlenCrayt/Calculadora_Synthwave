44fdd6397c23d45134abe0ae83356aeebb342c05
AlenCrayt <agus.santisteban@gmail.com>
2024-05-21 16:09:02 -0300

    Logs de consola eliminados, apretar C vacia tambien el arreglo linea_numeros ahora, agregado mas comentarios al codigo typescript, agregado propiedas en scss para esconder texto que excede la pantalla de la calculadora

--- script.ts
+++ script.ts
@@ -1,3 +1,4 @@
+//Codigo escrito por Agustin Santisteban, Usuario de Github: AlenCrayt
 document.addEventListener("DOMContentLoaded", () => {
   //Cuando el contenido del DOM se carga, esta asegurado de que los elementos button existen con lo cual los accedemos mediante querySelectorAll
   const botones_calculadora = document.querySelectorAll("button");
@@ -21,19 +22,27 @@ function push_a_lineas(valor: number): void {
   linea_visible.push(valor);
 }
 
-//Funcion que realiza la agrupacion de los numeros ingresados en linea_numeros en un conjunto y agrega ese conjunto al arreglo de conjuntos junto con el simbolo que se ingreso
+//Funcion que realiza la agrupacion de los numeros ingresados en linea_numeros en un conjunto y agrega ese conjunto al arreglo de conjuntos junto con el simbolo que se ingreso, toma como parametro el simbolo que el usario ingreso
 function agregar_conjuntos(simbolo: string): void {
+  //Se usa el metodo join para unir a todos los elementos numericos separados de linea_numeros en un solo conjunto de tipo string
   let conjunto = linea_numeros.join("");
+  //Se convierte al conjunto de numeros de Tipo string en uno de Tipo Number para hacer mas facil las operaciones futuras
   let numeros_juntos = Number(conjunto);
+  //Se agrega al final de linea_conjuntos el nuevo elemento numerico basado en numeros agrupados
   linea_conjuntos.push(numeros_juntos);
+  //Se agrega despues de los numeros el simbolo de tipo string para la operacion
   linea_conjuntos.push(simbolo);
+  //Se vacia linea_numeros para que este disponible por si hay una serie de numeros siguiente
   linea_numeros = [];
 }
 
 //Funcion que se encarga de realizar las operaciones matematicas necesarias a traves del arreglo linea_conjuntos y modifica este arreglos mediante iteracion hasta que solo queda el resultado de la operacion y un elemento vacio en el arreglo
 function calcular_resultado(): void {
+  //Creamos una simple variable numerica para tener los resultados de las operaciones
   let resultado: number;
+  //Usamos un iterador for para recorer el arreglo hasta llegar al fin(longitud del arreglo)
   for (let i = 0; i < linea_conjuntos.length; i++) {
+    //Sabemos que en el arreglo linea_conjuntos el indice 0 es un numero, el indice 1 es un simbolo y el indice 2 es otro numero con el cual se va realizar la operacion, en base a este conocimiento usamos switch para averiguar que tipo de simbolo hay en el indice 1 y dependiendo de lo que determine hacemos la operacion necesaria(suma, resta, division, multiplicacion)
     switch (linea_conjuntos[1]) {
       case "+":
         resultado = linea_conjuntos[0] + linea_conjuntos[2];
@@ -48,10 +57,11 @@ function calcular_resultado(): void {
         resultado = linea_conjuntos[0] / linea_conjuntos[2];
         break;
     }
+    //Cuando la operacion se ha realizado y tenemos el resultado, eliminamos los primeros tres elementos del arreglo(o sea los indices 0, 1 y 2) e insertamos al resultado al principio de linea_conjuntos con lo cual cuando se realiza la proxima operacion la hacemos con el resultado de la previa
     linea_conjuntos.splice(0, 3);
     linea_conjuntos.unshift(resultado);
-    console.log(linea_conjuntos);
   }
+  //Con un simple condicional averiguamos si la longitud del arreglo es 2 o menos(que significa que solo queda el resultado + un elemento vacio) y si es true entonces vaciamos el arreglo linea_visible y despues agregamos el resultado a este de esta manera mostrando al usario el resultado de la operacion que ingreso
   if (linea_conjuntos.length <= 2) {
     linea_visible = [];
     linea_visible.push(resultado);
@@ -67,6 +77,7 @@ function clickeo_boton(evento: MouseEvent) {
   //Accedemos a la "pantalla" de la calculadora
   const pantalla_calculadora = document.querySelector("#pantalla");
 
+  //Mediante switch determinamos cual es el valor del boton(en este caso es el contenido de texto en forma de string) y en base a eso se realiza la accion necesaria
   switch (valor_boton) {
     case "0":
       push_a_lineas(0);
@@ -121,9 +132,10 @@ function clickeo_boton(evento: MouseEvent) {
       break;
     case "C":
       linea_conjuntos = [];
+      linea_numeros = [];
       linea_visible = [];
       break;
   }
+  //EL contenido de la "pantalla" de la calculadora que es un elemento HTML h1 es asignado el valor mas actualizado de linea_visible despues de que el usuario presiona un boton de la calculadora
   pantalla_calculadora.textContent = linea_visible.join("");
-  console.log(linea_conjuntos);
 }
